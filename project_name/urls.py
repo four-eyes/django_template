1f0591bdcbc9d4499840c6a224eafd9e90408b49
Taras Lyapun <taraslyapun@gmail.com>
2014-12-18 17:19:12 +0200

    fixed project_name in urls and wsgi

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_template.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
 )
