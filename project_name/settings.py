9dd65153afb2cafb80df7abf02eb00aa10c4278a
Taras Lyapun <taraslyapun@gmail.com>
2014-12-19 11:32:07 +0200

    Unhardcoded SECRET_KEY

--- project_name/settings.py
+++ project_name/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'knh+u5-a7eg240pkm1kr_eai(b#+3=jrqq)hyyzt9%yixufo&5'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'knh+u5-a7eg240pkm1kr_eai(b#+3=jrqq)hyyzt9%yixufo&5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
